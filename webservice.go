bda766bcd9e6723890137f748dc615ade6c1f91b
Daniel Ortiz Pereira da Silva <daniel.particular@gmail.com>
2013-04-08 17:46:07 -0300

    init repository

---
+++ webservice.go
@@ -0,0 +1,88 @@
+package webservice
+
+import (
+	"encoding/xml"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type SoapIn interface {
+	GetAction() string
+}
+
+type SoapEnvelope struct {
+	XMLName  xml.Name `xml:"soap:Envelope"`
+	AttrXsi  string   `xml:"xmlns:xsi,attr"`
+	AttrXsd  string   `xml:"xmlns:xsd,attr"`
+	AttrSoap string   `xml:"xmlns:soap,attr"`
+	Body     SoapBody
+}
+
+func NewSoapEnvelope() *SoapEnvelope {
+	se := &SoapEnvelope{}
+	se.AttrXsi = "http://www.w3.org/2001/XMLSchema-instance"
+	se.AttrXsd = "http://www.w3.org/2001/XMLSchema"
+	se.AttrSoap = "http://schemas.xmlsoap.org/soap/envelope/"
+
+	return se
+}
+
+type SoapBody struct {
+	XMLName xml.Name `xml:"soap:Body"`
+	Content string   `xml:",innerxml"`
+}
+
+func callService(si SoapIn, url string) (sr *SoapEnvelope, err error) {
+	// cria o soap envelope
+	se := NewSoapEnvelope()
+
+	// gerar o conteúdo do corpo em xml
+	bsi, err := xml.Marshal(&si)
+	if err != nil {
+		return nil, err
+	}
+
+	// associa o corpo da requisição
+	se.Body.Content = string(bsi)
+
+	// gerar o xml da requisição
+	bse, err := xml.Marshal(&se)
+	if err != nil {
+		return nil, err
+	}
+
+	// cria um reader para o corpo da requisição
+	br := strings.NewReader(string(bse))
+
+	// cria a requisição
+	req, err := http.NewRequest("POST", url, br)
+	if err != nil {
+		return nil, err
+	}
+
+	// adiciona os cabeçalhos http necessários do soap
+	req.Header.Add("Content-Type", "text/xml; charset=utf-8")
+	req.Header.Add("SOAPAction", si.GetAction())
+
+	// executa a requisição
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// le o conteudo do retorno
+	bsr, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// gerar a estrutura de retorno
+	err = xml.Unmarshal(bsr, &sr)
+	if err != nil {
+		return nil, err
+	}
+	
+	return sr, nil
+}
---
+++ webservice_test.go
@@ -0,0 +1,119 @@
+package webservice
+
+import (
+	"testing"
+	"encoding/xml"
+)
+
+type ObterUsuariosSoapIn struct {
+	XMLName      xml.Name        `xml:"obterUsuarios"`
+	XMLNamespace string          `xml:"xmlns,attr"`
+	Action       string          `xml:"-"`
+	Param        UsuarioParamDTO `xml:"param"`
+}
+
+func (si ObterUsuariosSoapIn) GetAction() string {
+	return si.Action
+}
+
+type ObterUsuariosSoapOut struct {
+	XMLName      xml.Name          `xml:"obterUsuariosResponse"`
+	XMLNamespace string            `xml:"xmlns,attr"`
+	Return       UsuarioResultList `xml:"return"`
+}
+
+func NewObterUsuariosSoapIn() ObterUsuariosSoapIn {
+	si := ObterUsuariosSoapIn{}
+	si.XMLNamespace = "http://webservice.auth.app.bsbr.altec.com/"
+	si.Action = ""
+
+	return si
+}
+
+type UsuarioParamDTO struct {
+	XMLNamespace string `xml:"xmlns,attr"`
+	Agencia      string `xml:"agencia"`
+	Conta        string `xml:"conta"`
+	Banco        string `xml:"banco"`
+	Imei         string `xml:"IMEI"`
+	Linha        string `xml:"linha"`
+}
+
+type UsuarioResultList struct {
+	Agencia       string    `xml:"agencia"`
+	Conta         string    `xml:"conta"`
+	Banco         string    `xml:"banco"`
+	CodErro       string    `xml:"codErro"`
+	MsgErro       string    `xml:"msgErro"`
+	TipoConta     string    `xml:"tipoConta"`
+	ListaUsuarios []Usuario `xml:"listaUsuarios"`
+}
+
+type Usuario struct {
+	CodigoUsuario      string `xml:"codigoUsuario"`
+	CodigoUsuarioConta string `xml:"codigoUsuarioConta"`
+	FlagCadastro       string `xml:"flagCadastro"`
+	HsEkscPbe          string `xml:"hsEkscPbe"`
+	Nome               string `xml:"nome"`
+	Penumper           string `xml:"penumper"`
+	TipoUsuario        string `xml:"tipoUsuario"`
+	SKa                string `xml:"sKa"`
+	SRand              string `xml:"sRand"`
+	SSingJs            string `xml:"sSignJs"`
+}
+
+// http://localhost/Santander/Webservices/AuthService/AuthEndpointService.asmx
+
+type AuthEndpointService struct {
+	Url string
+}
+
+func NewAuthEndpointService() *AuthEndpointService {
+	s := &AuthEndpointService{}
+	s.Url = "http://localhost/Santander/Webservices/AuthService/AuthEndpointService.asmx"
+	
+	return s	
+}
+
+// função de chamada de exemplo
+func (s AuthEndpointService) ObterUsuarios(param UsuarioParamDTO) (r *UsuarioResultList, err error) {
+	si := NewObterUsuariosSoapIn()
+	si.Param = param
+
+	// chama o serviço apontando para determina url
+	sr, err := callService(si, s.Url)
+	if err != nil {
+		return nil, err
+	}
+
+	// monta a estrutura de retorno
+	var so ObterUsuariosSoapOut
+	err = xml.Unmarshal([]byte(sr.Body.Content), &so)
+	if err != nil {
+		return nil, err
+	}
+
+	return &so.Return, nil
+}
+
+
+func TestAuthEndpointService(t *testing.T) {
+	s := NewAuthEndpointService()
+
+	p := UsuarioParamDTO{}
+	p.Banco = "0033"
+	p.Agencia = "1111"
+	p.Conta = "000111111111"
+	p.Imei = "01020304"
+	p.Linha = "983157131"
+
+	l, err := s.ObterUsuarios(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if c := len(l.ListaUsuarios); c != 2 {
+		t.Fatalf("esperado 2 usuarios recebido %d usuarios", c)
+	}
+
+}
